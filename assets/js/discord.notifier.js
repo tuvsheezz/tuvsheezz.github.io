cf460c0d927921c5d6320ac9d7f6f72c71ab8207
tuvsheezz <tuvshee0089@gmail.com>
2022-05-18 21:29:05 +0900

    make it better2

--- assets/js/discord.notifier.js
+++ assets/js/discord.notifier.js
@@ -21,9 +21,11 @@ setInterval(function() {
 
   console.log(today)
   // readTextFile()
+  const query2 = new URLSearchParams(window.location.search);
+  const webhookURL = query2.get('discord')
 
   const request = new XMLHttpRequest();
-  request.open("POST", "https://discord.com/api/webhooks/976119401931735070/4PMnZC7qDqENRWkNGIf6o47sMYjY-FCskkrdIpf7eQ-T71TDB0e_JwRyF_DLGawJZL4H");
+  request.open("POST", webhookURL);
 
   request.setRequestHeader('Content-type', 'application/json');
   const params = { content: "HI" }
